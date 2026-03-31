f40f02f738c5a19c52e0fa93521c0fa75b19a6ba
HagbardCeline <janpauldahlke@gmail.com>
2026-03-31 17:27:05 +0200

    before testing

--- test_qdrant.rs
+++
@@ -1,9 +0,0 @@
-use qdrant_client::qdrant::{UpsertPointsBuilder, SearchPointsBuilder, PointStruct, VectorParams, Distance};
-use std::collections::HashMap;
-
-fn test() {
-    let p = PointStruct::new("id".to_string(), vec![1.0], HashMap::new());
-    let u = UpsertPointsBuilder::new("coll", vec![p]);
-    
-    let s = SearchPointsBuilder::new("coll", vec![1.0], 10).with_payload(true);
-}
